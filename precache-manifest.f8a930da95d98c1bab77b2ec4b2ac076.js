e6faff9acfa9e6d29c023c256d334a2d2a74d099
Taras-Rm <tarasrom25@gmail.com>
2021-06-01 14:01:20 +0300

    Updates

---
+++ precache-manifest.f8a930da95d98c1bab77b2ec4b2ac076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae25ed915865d8698380690dd48d9ae",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "88f0890d05528287f216",
+    "url": "/react-to-do-list/static/css/main.797f4a5d.chunk.css"
+  },
+  {
+    "revision": "16a7adf2dba469e30194",
+    "url": "/react-to-do-list/static/js/2.3168fa0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-list/static/js/2.3168fa0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f0890d05528287f216",
+    "url": "/react-to-do-list/static/js/main.a7816df2.chunk.js"
+  },
+  {
+    "revision": "aecbfdc557e7c6997b64",
+    "url": "/react-to-do-list/static/js/runtime-main.e2325aac.js"
+  }
+]);
