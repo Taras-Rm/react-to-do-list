c177495a8376af19bc3208042c5f3deb09a8f28e
Taras-Rm <tarasrom25@gmail.com>
2021-06-01 11:57:50 +0300

    add server.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends React.Component {
     this.state = {
       toDoListItems: mainState,
       inputText: '',
-      activeItemsCount: mainState.filter(item => item.isDone === false ? item : null).length
-    }
+      activeItemsCount: mainState.filter((item) => (item.isDone === false ? item : null)).length,
+    };
 
     this.changeItemStatus = this.changeItemStatus.bind(this);
     this.addItem = this.addItem.bind(this);
@@ -22,79 +22,83 @@ class App extends React.Component {
 
   // change item status method
   changeItemStatus(itemId) {
-    this.setState(prevState => {
-      const changedItemsArr = prevState.toDoListItems.map(item => {
+    this.setState((prevState) => {
+      const changedItemsArr = prevState.toDoListItems.map((item) => {
         if (item.id === itemId) {
-          return{
+          return {
             ...item,
-            isDone: !item.isDone
-          }
+            isDone: !item.isDone,
+          };
         }
         return item;
-      })
-      let countArr = changedItemsArr.filter(item => item.isDone === false ? item : null);
+      });
+      let countArr = changedItemsArr.filter((item) => (item.isDone === false ? item : null));
       return {
         toDoListItems: changedItemsArr,
-        activeItemsCount: countArr.length
-      }
-    })
+        activeItemsCount: countArr.length,
+      };
+    });
   }
 
   // update input text with input
   // input.inputText contain current input text
   updateInputText(inputText) {
-    this.setState({inputText: inputText})
+    this.setState({ inputText: inputText });
   }
 
   // add item method
   addItem() {
-    this.setState(prevState => {
-        const newItemsArr = [...prevState.toDoListItems];
-      
-        newItemsArr.unshift({id: Math.round(Math.random()*10000), text: this.state.inputText, isDone: false});
-        
-        return{
-          toDoListItems: newItemsArr,
-          activeItemsCount: prevState.activeItemsCount++,
-          inputText: ''
-        }
-    })
+    this.setState((prevState) => {
+      const newItemsArr = [...prevState.toDoListItems];
+
+      newItemsArr.unshift({
+        id: Math.round(Math.random() * 10000),
+        text: this.state.inputText,
+        isDone: false,
+      });
+
+      return {
+        toDoListItems: newItemsArr,
+        activeItemsCount: prevState.activeItemsCount++,
+        inputText: '',
+      };
+    });
   }
 
   // delete item method
   deleteItem(itemId) {
-    this.setState(prevState => {
-      const newItemsArr2 = prevState.toDoListItems.filter(item => {
-        if(item.id !== itemId){
-          return {...item};
+    this.setState((prevState) => {
+      const newItemsArr2 = prevState.toDoListItems.filter((item) => {
+        if (item.id !== itemId) {
+          return { ...item };
         } else {
-          return null
+          return null;
         }
-      })
-      let countArr = newItemsArr2.filter(item => item.isDone === false ? item : null);
+      });
+      let countArr = newItemsArr2.filter((item) => (item.isDone === false ? item : null));
       return {
         toDoListItems: newItemsArr2,
-        activeItemsCount: countArr.length
-      }
-    })
+        activeItemsCount: countArr.length,
+      };
+    });
   }
 
   render() {
     return (
-    <div className="App">
-      <h2 className="headerTitle">To do:</h2>
-      <span className="activeItem">Active items: {this.state.activeItemsCount}</span>
-      <ToDoList 
-      changeItemStatus={this.changeItemStatus} 
-      toDoListItems={this.state.toDoListItems}
-      deleteItem={this.deleteItem}
-      />
-      <AddItem 
-      inputText={this.state.inputText} 
-      addItem={this.addItem}
-      updateInputText ={this.updateInputText}
-      />
-    </div>
+      <div className="App">
+        <h2 className="headerTitle">To do:</h2>
+        <span className="activeItem">Active items: {this.state.activeItemsCount}</span>
+        <ToDoList
+          changeItemStatus={this.changeItemStatus}
+          toDoListItems={this.state.toDoListItems}
+          deleteItem={this.deleteItem}
+        />
+        <AddItem
+          inputText={this.state.inputText}
+          addItem={this.addItem}
+          updateInputText={this.updateInputText}
+        />
+      </div>
     );
   }
 }
