9ee9b0494f42301fb469c046e13dad84d549c9b6
Taras-Rm <tarasrom25@gmail.com>
2021-06-01 13:46:48 +0300

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
